f04ae0e4be5d80766e111d77a9873a0f4dec32ae
V. Arun <arun@cs.umass.edu>
2023-11-04 19:06:16 -0400

    Added a force exit at the end.

--- test/GraderSingleServerGradescope.java
+++ test/GraderSingleServerGradescope.java
@@ -1,3 +1,4 @@
+import com.gradescope.jh61b.grader.GradedTest;
 import com.gradescope.jh61b.grader.GradedTestListenerJSON;
 import org.junit.*;
 import org.junit.runner.JUnitCore;
@@ -8,15 +9,12 @@ import java.io.IOException;
 @FixMethodOrder(org.junit.runners.MethodSorters.NAME_ASCENDING)
 public class GraderSingleServerGradescope extends GraderSingleServer {
 
+
     public static void main(String[] args) throws IOException {
         JUnitCore runner = new JUnitCore();
-        runner.addListener(new GradedTestListenerJSON());
+        GradedTestListenerJSON gradedTestListenerJSON;
+        runner.addListener(gradedTestListenerJSON=new GradedTestListenerJSON());
         Result r = runner.run(GraderSingleServer.class);
-
-//        Result result = JUnitCore.runClasses(GraderSingleServerGradescope.class);
-//        for (Failure failure : result.getFailures()) {
-//            System.out.println(failure.toString());
-//            failure.getException().printStackTrace();
-//        }
+        System.exit(0);
     }
 }
